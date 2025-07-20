996ec9342ec0d88e3512aa395d58f473ba112522
kimhyunkwang <khkhy8712@gmail.com>
2025-07-20 15:40:36 +0900

    Feat: csv 데이터 파일 불러오기

---
+++ caffee_map.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+area_map = pd.read_csv('./dataFile/area_map.csv')
+area_struct = pd.read_csv('./dataFile/area_struct.csv')
+area_category = pd.read_csv('./dataFile/area_category.csv')
